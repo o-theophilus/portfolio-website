664d153479fd0b0859d0db743bd8612cb249f37f
theophilus <theophilus.ogbolu@gmail.com>
2023-01-06 06:09:15 +0100

    fixed

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,14 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
-// import vercel from '@sveltejs/adapter-vercel';
 
 const config = {
 	kit: {
 		adapter: adapter()
-		// adapter: vercel({
-		// 	edge: false,
-		// 	external: [],
-		// 	split: false
-		//   })
 	}
 };
 
