17d5d91fa04c69c46efae91ebc6fa3e6e04b87a5
theophilus <theophilus.ogbolu@gmail.com>
2023-04-19 19:10:15 +0100

    test 5

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script>
-	// import { _user } from '$lib/store.js';
-	// import { token } from '$lib/cookie.js';
+	import { _user } from '$lib/store.js';
+	import { token } from '$lib/cookie.js';
 
-	// import './css/var.css';
-	// import './css/main.css';
-	// import './css/form.css';
+	import './css/var.css';
+	import './css/main.css';
+	import './css/form.css';
 
-	// import Module from '$lib/module/module.svelte';
-	// import Footer from './footer/footer.svelte';
-	// import Loading from '$lib/comp/loading.svelte';
+	import Module from '$lib/module/module.svelte';
+	import Footer from './footer/footer.svelte';
+	import Loading from '$lib/comp/loading.svelte';
 
 	// export let data;
 	// $_user = data.data.locals.user;
