52a8787bdedc12a49bad3871de19c232ab82f736
theophilus <theophilus.ogbolu@gmail.com>
2023-04-19 21:05:40 +0100

    test34

--- frontend/src/hooks.server.js
+++ frontend/src/hooks.server.js
@@ -22,11 +22,18 @@ export async function handle({ event, resolve }) {
 
     // throw new Error(404, `Error status: ${resp.status}`)
     event.locals = {
-        user: {
-            setting: {
-                theme: 'dark'
-            }
-        }
+        "token": "IjFkYjk0ZDhjMDA0NjRhNWY5OWFkMTdlZDNhOTJhNTk4Ig.ZEBIgA.s-Z2erT0a9yHfcEhECesdVJ5CpQ",
+        "user": {
+            "email": "f2855b041579480a957cf2ba645ed413",
+            "key": "1db94d8c00464a5f99ad17ed3a92a598",
+            "login": false,
+            "name": "f2855b041579480a957cf2ba645ed413",
+            "roles": [],
+            "setting": {
+                "theme": "dark"
+            },
+            "status": "anonymous"
+        },
     };
     return await resolve(event);
 }
