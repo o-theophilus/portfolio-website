fe4db4fefe70d586ed4a2fd8eac2edbb6237b06a
theophilus <theophilus.ogbolu@gmail.com>
2023-04-19 19:14:08 +0100

    test7

--- frontend/src/hooks.server.js
+++ frontend/src/hooks.server.js
@@ -2,33 +2,33 @@ import { api_url } from '$lib/store.js';
 
 export async function handle({ event, resolve }) {
 
-    // const resp = await fetch(`${api_url}/init`, {
-    //     method: 'post',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         Authorization: event.cookies.get("token")
-    //     }
-    // });
-
-    // if (resp.ok) {
-    //     let data = await resp.json();
-
-    //     if (data.status == 200) {
-    //         // event.cookies.set("session", data.data.token)
-    //         event.locals = data.data;
-    //     }
-    //     return await resolve(event);
-    // }
-
-    // throw new Error(404, `Error status: ${resp.status}`)
-    event.locals = {
-        user: {
-            setting: {
-                theme: 'dark'
-            }
+    const resp = await fetch(`${api_url}/init`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: event.cookies.get("token")
         }
-    };
-    return await resolve(event);
+    });
+
+    if (resp.ok) {
+        let data = await resp.json();
+
+        if (data.status == 200) {
+            // event.cookies.set("session", data.data.token)
+            event.locals = data.data;
+        }
+        return await resolve(event);
+    }
+
+    throw new Error(404, `Error status: ${resp.status}`)
+    // event.locals = {
+    //     user: {
+    //         setting: {
+    //             theme: 'dark'
+    //         }
+    //     }
+    // };
+    // return await resolve(event);
 }
 
 
