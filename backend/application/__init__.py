c98bf052ec359944c4f0f9c29d6402791ca8c0c3
theophilus <theophilus.ogbolu@gmail.com>
2023-04-20 22:14:52 +0100

    working graphQL

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -1,15 +1,58 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from graphene import ObjectType, String, Schema, List, Mutation
+from .api import db
+from flask_graphql import GraphQLView
 
-from .api import bp as api
-from .api.post import bp as post
-from .api.post_get import bp as post_get
-from .api.post_comment import bp as comment
-from .api.photo import bp as photo
-from .api.tag import bp as tag
-from .api.user import bp as user
-from .api.auth import bp as auth
-from .api.auth_password_forgot import bp as auth_password_forgot
+
+class Item(ObjectType):
+    name = String()
+    type = String()
+
+
+class Query(ObjectType):
+    users = List(Item)
+    ttt = List(Item)
+
+    def resolve_users(root, info):
+        return [Item(
+            name=x.get('name'),
+            type=x.get('type')
+        ) for x in db.data()]
+
+    def resolve_ttt(root, info):
+        return [Item(
+            name=x.get('name'),
+            type=x.get('type')
+        ) for x in db.data()]
+
+
+class CreateUser(Mutation):
+    class Arguments:
+        name = String(required=True)
+        age = String(required=True)
+
+    success = String()
+    user = String()
+
+    def mutate(self, info, name, age):
+        # Code to create a new user in the database goes here
+        # ...
+
+        # db.put({'name': name, 'age': age})
+        # print({'name': name, 'age': age})
+
+        success = True
+        user = f'User {name} created with age {age}'
+
+        return CreateUser(success=success, user=user)
+
+
+class Mutations(ObjectType):
+    create_user = CreateUser.Field()
+
+
+schema = Schema(query=Query, mutation=Mutations)
 
 
 def create_app(config_file="config.py"):
@@ -17,14 +60,21 @@ def create_app(config_file="config.py"):
     app.config.from_pyfile(config_file)
     CORS(app)
 
-    app.register_blueprint(api)
-    app.register_blueprint(post)
-    app.register_blueprint(post_get)
-    app.register_blueprint(comment)
-    app.register_blueprint(photo)
-    app.register_blueprint(user)
-    app.register_blueprint(tag)
-    app.register_blueprint(auth)
-    app.register_blueprint(auth_password_forgot)
+    app.add_url_rule(
+        '/graphql',
+        view_func=GraphQLView.as_view(
+            'graphql',
+            schema=schema,
+            graphiql=True
+        ))
+
+    # @app.post('/graphql')
+    # def graphql():
+    #     data = request.get_json()
+    #     result = schema.execute(data['query'])
+    #     print(type(result))
+    #     print(dir(result))
+    #     print(result.to_dict)
+    #     return jsonify(result.data)
 
     return app
